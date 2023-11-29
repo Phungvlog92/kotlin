065d77dea72b6f85e145b76f3e252a226330bc42
Nikolay Lunyak <nikolay.lunyak@jetbrains.com>
2023-11-29 11:53:42 +0200

    fixup! [FIR] Don't copy VP default values for intersection overrides
    Stop copying default values for
    `Enhancement`. Nothing seems to fail,
    so this feels reasonable.

--- compiler/fir/checkers/checkers.jvm/src/org/jetbrains/kotlin/fir/analysis/jvm/checkers/declaration/FirOverrideJavaNullabilityWarningChecker.kt
+++ compiler/fir/checkers/checkers.jvm/src/org/jetbrains/kotlin/fir/analysis/jvm/checkers/declaration/FirOverrideJavaNullabilityWarningChecker.kt
@@ -126,7 +126,6 @@ private fun FirSimpleFunction.substituteOrNull(
             newReturnType = newReturnType,
             newContextReceiverTypes = newContextReceiverTypes,
             newReceiverType = newExtensionReceiverType,
-            copyDefaultValues = true,
         )
     }
 }
