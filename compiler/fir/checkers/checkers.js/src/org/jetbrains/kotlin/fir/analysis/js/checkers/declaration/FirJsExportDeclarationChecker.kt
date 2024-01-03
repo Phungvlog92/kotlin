e83099a23b4ea965b24da7d474b5bfd6b0f25bec
Artem Kobzar <artem.kobzar@jetbrains.com>
2024-01-03 10:35:32 +0100

    Fix nullable unsigned arrays exportability after the last rebasing

--- compiler/fir/checkers/checkers.js/src/org/jetbrains/kotlin/fir/analysis/js/checkers/declaration/FirJsExportDeclarationChecker.kt
+++ compiler/fir/checkers/checkers.js/src/org/jetbrains/kotlin/fir/analysis/js/checkers/declaration/FirJsExportDeclarationChecker.kt
@@ -223,7 +223,6 @@ object FirJsExportDeclarationChecker : FirBasicDeclarationChecker() {
                 || isNothingOrNullableNothing
                 || isPrimitiveArray
                 || isNonPrimitiveArray
-                || isArrayType
                 || isList
                 || isMutableList
                 || isSet
