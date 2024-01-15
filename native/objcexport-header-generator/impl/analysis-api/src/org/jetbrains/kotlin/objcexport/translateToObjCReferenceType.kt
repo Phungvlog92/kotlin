6f3571a93fc25ca6bd2d9e4bd8170f8ea1d6a9da
eugene.levenetc <eugene.levenetc@jetbrains.com>
2024-01-15 13:18:07 +0100

    Add hidden types mapping

--- native/objcexport-header-generator/impl/analysis-api/src/org/jetbrains/kotlin/objcexport/translateToObjCReferenceType.kt
+++ native/objcexport-header-generator/impl/analysis-api/src/org/jetbrains/kotlin/objcexport/translateToObjCReferenceType.kt
@@ -5,6 +5,7 @@ import org.jetbrains.kotlin.analysis.api.types.KtType
 import org.jetbrains.kotlin.backend.konan.objcexport.*
 import org.jetbrains.kotlin.builtins.StandardNames
 import org.jetbrains.kotlin.name.ClassId
+import org.jetbrains.kotlin.name.FqName
 
 /**
  * [org.jetbrains.kotlin.backend.konan.objcexport.ObjCExportTranslatorImpl.mapReferenceType]
@@ -20,30 +21,40 @@ internal fun KtType.translateToObjCReferenceType(): ObjCReferenceType {
 context(KtAnalysisSession, KtObjCExportSession)
 private fun KtType.mapToReferenceTypeIgnoringNullability(): ObjCNonNullReferenceType {
 
-    /**
-     * Simplified version of [org.jetbrains.kotlin.backend.konan.objcexport.CustomTypeMapper]
-     */
-    val typesMap = mutableMapOf<ClassId, String>().apply {
-        this[ClassId.topLevel(StandardNames.FqNames.list)] = "NSArray"
-        this[ClassId.topLevel(StandardNames.FqNames.mutableList)] = "NSMutableArray"
-        this[ClassId.topLevel(StandardNames.FqNames.set)] = "NSSet"
-        this[ClassId.topLevel(StandardNames.FqNames.mutableSet)] = "MutableSet".getObjCKotlinStdlibClassOrProtocolName().objCName
-        this[ClassId.topLevel(StandardNames.FqNames.map)] = "NSDictionary"
-        this[ClassId.topLevel(StandardNames.FqNames.mutableMap)] = "MutableDictionary".getObjCKotlinStdlibClassOrProtocolName().objCName
-        this[ClassId.topLevel(StandardNames.FqNames.string.toSafe())] = "NSString"
-    }
+    val classId = this.expandedClassSymbol?.classIdIfNonLocal
 
-    NSNumberKind.entries.forEach {
-        val classId = it.mappedKotlinClassId
-        if (classId != null) {
-            typesMap[classId] = classId.shortClassName.asString().getObjCKotlinStdlibClassOrProtocolName().objCName
+    val isAny = false //TODO: KotlinBuiltIns.isAny(classDescriptor)
+    val isInlined = false //TODO: classDescriptor.isInlined()
+    val isHidden = classId in hiddenTypes
+
+    return if (isAny || isHidden || isInlined) {
+        ObjCIdType
+    } else {
+        /**
+         * Simplified version of [org.jetbrains.kotlin.backend.konan.objcexport.CustomTypeMapper]
+         */
+        val typesMap = mutableMapOf<ClassId, String>().apply {
+            this[ClassId.topLevel(StandardNames.FqNames.list)] = "NSArray"
+            this[ClassId.topLevel(StandardNames.FqNames.mutableList)] = "NSMutableArray"
+            this[ClassId.topLevel(StandardNames.FqNames.set)] = "NSSet"
+            this[ClassId.topLevel(StandardNames.FqNames.mutableSet)] = "MutableSet".getObjCKotlinStdlibClassOrProtocolName().objCName
+            this[ClassId.topLevel(StandardNames.FqNames.map)] = "NSDictionary"
+            this[ClassId.topLevel(StandardNames.FqNames.mutableMap)] = "MutableDictionary".getObjCKotlinStdlibClassOrProtocolName().objCName
+            this[ClassId.topLevel(StandardNames.FqNames.string.toSafe())] = "NSString"
         }
+
+        NSNumberKind.entries.forEach {
+            val classId = it.mappedKotlinClassId
+            if (classId != null) {
+                typesMap[classId] = classId.shortClassName.asString().getObjCKotlinStdlibClassOrProtocolName().objCName
+            }
+        }
+
+        val typeName = typesMap[classId]
+            ?: throw IllegalStateException("Unsupported mapping type for $this")
+
+        ObjCClassType(typeName)
     }
-
-    val typeName = typesMap[this.expandedClassSymbol?.classIdIfNonLocal]
-        ?: throw IllegalStateException("Unsupported mapping type for $this")
-
-    return ObjCClassType(typeName)
 }
 
 private fun ObjCNonNullReferenceType.withNullabilityOf(kotlinType: KtType): ObjCReferenceType {
@@ -53,3 +64,15 @@ private fun ObjCNonNullReferenceType.withNullabilityOf(kotlinType: KtType): ObjC
         this
     }
 }
+
+private val hiddenTypes: Set<ClassId> = listOf(
+    "kotlin.Any",
+    "kotlin.CharSequence",
+    "kotlin.Comparable",
+    "kotlin.Function",
+    "kotlin.Number",
+    "kotlin.collections.Collection",
+    "kotlin.collections.Iterable",
+    "kotlin.collections.MutableCollection",
+    "kotlin.collections.MutableIterable"
+).map { ClassId.topLevel(FqName(it)) }.toSet()
