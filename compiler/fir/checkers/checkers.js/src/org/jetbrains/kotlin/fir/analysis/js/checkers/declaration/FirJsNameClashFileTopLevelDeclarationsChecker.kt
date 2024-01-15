323115b08f8976e31fbab61514436f92ad2db67d
Alexander Korepanov <alexander.korepanov@jetbrains.com>
2024-01-15 15:38:03 +0100

    [JS FIR] Rework JS_NAME_CLASH diagnostic for constructors
     - Fix a false positive JS_NAME_CLASH report on constructors
       from different classes but with the same JsName.
     - Allow the same JsName for class constructors and other top-level
       declarations. This behavior differs from K1, but it is correct
       since there is no real name clash in the generated JS code.
    
    ^KT-64867 Fixed

--- compiler/fir/checkers/checkers.js/src/org/jetbrains/kotlin/fir/analysis/js/checkers/declaration/FirJsNameClashFileTopLevelDeclarationsChecker.kt
+++ compiler/fir/checkers/checkers.js/src/org/jetbrains/kotlin/fir/analysis/js/checkers/declaration/FirJsNameClashFileTopLevelDeclarationsChecker.kt
@@ -41,11 +41,6 @@ object FirJsNameClashFileTopLevelDeclarationsChecker : FirFileChecker() {
                 continue
             }
             topLevelDeclarationsWithStableName.addStableName(topLevelDeclaration.symbol, context)
-            if (topLevelDeclaration is FirClass) {
-                for (classConstructor in topLevelDeclaration.constructors(context.session)) {
-                    topLevelDeclarationsWithStableName.addStableName(classConstructor, context)
-                }
-            }
         }
         for ((name, stableNames) in topLevelDeclarationsWithStableName.entries) {
             for (symbol in stableNames) {
