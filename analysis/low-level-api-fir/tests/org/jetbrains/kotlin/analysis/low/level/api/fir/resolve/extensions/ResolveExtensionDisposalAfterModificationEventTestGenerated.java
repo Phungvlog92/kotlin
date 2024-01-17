GlebSolovev/kmm-research

[LL FIR] Test resolve extension disposal after modification events
- These tests simply ensure that a resolve extension is disposed after
  any kind of modification event is raised. It's not meant to test
  complex scenarios, but rather to detect when resolve extension
  disposal isn't invoked *at all*.
- I tried implementing a similar test for resolve extension disposal
  after soft reference garbage collection, but the only way to force the
  GC to collect soft references is to allocate memory until an
  out-of-memory error occurs. That is bad for the test infrastructure,
  because it might allocate A LOT of memory (depending on the max heap),
  which is problematic for running tests locally. Also, our Kotlin tests
  stop on an OOM error altogether (so additional configuration would be
  required) and the heap is dumped into a 20GB file (on my machine),
  which is again problematic for local test runs.

^KT-61222

analysis/low-level-api-fir/tests/org/jetbrains/kotlin/analysis/low/level/api/fir/resolve/extensions/ResolveExtensionDisposalAfterModificationEventTestGenerated.java
@@ -1,0 +1,56 @@
+/*
+ * Copyright 2010-2023 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.analysis.low.level.api.fir.resolve.extensions;
+
+import com.intellij.testFramework.TestDataPath;
+import org.jetbrains.kotlin.test.util.KtTestUtil;
+import org.jetbrains.kotlin.test.TestMetadata;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.util.regex.Pattern;
+
+/** This class is generated by {@link org.jetbrains.kotlin.generators.tests.analysis.api.GenerateAnalysisApiTestsKt}. DO NOT MODIFY MANUALLY */
+@SuppressWarnings("all")
+@TestMetadata("analysis/low-level-api-fir/testdata/resolveExtensionDisposal")
+@TestDataPath("$PROJECT_ROOT")
+public class ResolveExtensionDisposalAfterModificationEventTestGenerated extends AbstractResolveExtensionDisposalAfterModificationEventTest {
+    @Test
+    @TestMetadata("afterGlobalModuleStateModification.kt")
+    public void testAfterGlobalModuleStateModification() throws Exception {
+        runTest("analysis/low-level-api-fir/testdata/resolveExtensionDisposal/afterGlobalModuleStateModification.kt");
+    }
+
+    @Test
+    @TestMetadata("afterGlobalSourceModuleStateModification.kt")
+    public void testAfterGlobalSourceModuleStateModification() throws Exception {
+        runTest("analysis/low-level-api-fir/testdata/resolveExtensionDisposal/afterGlobalSourceModuleStateModification.kt");
+    }
+
+    @Test
+    @TestMetadata("afterGlobalSourceOutOfBlockModification.kt")
+    public void testAfterGlobalSourceOutOfBlockModification() throws Exception {
+        runTest("analysis/low-level-api-fir/testdata/resolveExtensionDisposal/afterGlobalSourceOutOfBlockModification.kt");
+    }
+
+    @Test
+    @TestMetadata("afterModuleOutOfBlockModification.kt")
+    public void testAfterModuleOutOfBlockModification() throws Exception {
+        runTest("analysis/low-level-api-fir/testdata/resolveExtensionDisposal/afterModuleOutOfBlockModification.kt");
+    }
+
+    @Test
+    @TestMetadata("afterModuleStateModification.kt")
+    public void testAfterModuleStateModification() throws Exception {
+        runTest("analysis/low-level-api-fir/testdata/resolveExtensionDisposal/afterModuleStateModification.kt");
+    }
+
+    @Test
+    public void testAllFilesPresentInResolveExtensionDisposal() throws Exception {
+        KtTestUtil.assertAllTestsPresentByMetadataWithExcluded(this.getClass(), new File("analysis/low-level-api-fir/testdata/resolveExtensionDisposal"), Pattern.compile("^(.+)\\.kt$"), null, true);
+    }
+}

Phungvlog92/kotlin

analysis/low-level-api-fir/tests/org/jetbrains/kotlin/analysis/low/level/api/fir/resolve/extensions/ResolveExtensionDisposalAfterModificationEventTestGenerated.java
@@ -1,0 +1,56 @@
+/*
+ * Copyright 2010-2023 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.analysis.low.level.api.fir.resolve.extensions;
+
+import com.intellij.testFramework.TestDataPath;
+import org.jetbrains.kotlin.test.util.KtTestUtil;
+import org.jetbrains.kotlin.test.TestMetadata;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.util.regex.Pattern;
+
+/** This class is generated by {@link org.jetbrains.kotlin.generators.tests.analysis.api.GenerateAnalysisApiTestsKt}. DO NOT MODIFY MANUALLY */
+@SuppressWarnings("all")
+@TestMetadata("analysis/low-level-api-fir/testData/resolveExtensionDisposal")
+@TestDataPath("$PROJECT_ROOT")
+public class ResolveExtensionDisposalAfterModificationEventTestGenerated extends AbstractResolveExtensionDisposalAfterModificationEventTest {
+    @Test
+    @TestMetadata("afterGlobalModuleStateModification.kt")
+    public void testAfterGlobalModuleStateModification() throws Exception {
+        runTest("analysis/low-level-api-fir/testData/resolveExtensionDisposal/afterGlobalModuleStateModification.kt");
+    }
+
+    @Test
+    @TestMetadata("afterGlobalSourceModuleStateModification.kt")
+    public void testAfterGlobalSourceModuleStateModification() throws Exception {
+        runTest("analysis/low-level-api-fir/testData/resolveExtensionDisposal/afterGlobalSourceModuleStateModification.kt");
+    }
+
+    @Test
+    @TestMetadata("afterGlobalSourceOutOfBlockModification.kt")
+    public void testAfterGlobalSourceOutOfBlockModification() throws Exception {
+        runTest("analysis/low-level-api-fir/testData/resolveExtensionDisposal/afterGlobalSourceOutOfBlockModification.kt");
+    }
+
+    @Test
+    @TestMetadata("afterModuleOutOfBlockModification.kt")
+    public void testAfterModuleOutOfBlockModification() throws Exception {
+        runTest("analysis/low-level-api-fir/testData/resolveExtensionDisposal/afterModuleOutOfBlockModification.kt");
+    }
+
+    @Test
+    @TestMetadata("afterModuleStateModification.kt")
+    public void testAfterModuleStateModification() throws Exception {
+        runTest("analysis/low-level-api-fir/testData/resolveExtensionDisposal/afterModuleStateModification.kt");
+    }
+
+    @Test
+    public void testAllFilesPresentInResolveExtensionDisposal() throws Exception {
+        KtTestUtil.assertAllTestsPresentByMetadataWithExcluded(this.getClass(), new File("analysis/low-level-api-fir/testData/resolveExtensionDisposal"), Pattern.compile("^(.+)\\.kt$"), null, true);
+    }
+}