98badad490bf6b210e65fc633706c4b1ca196b7f
Alexander Likhachev <Alexander.Likhachev@jetbrains.com>
2024-01-18 20:10:52 +0000

    fixup! [Gradle, JS] Add test with yarn.lock checks

--- libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/Kotlin2JsGradlePluginIT.kt
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/Kotlin2JsGradlePluginIT.kt
@@ -1267,7 +1267,7 @@ class Kotlin2JsIrGradlePluginIT : KGPBaseTest() {
                 val react = "react"
                 val reactVersion = getPackageJson().overrides?.get(react)
                 val requiredReactVersion = "16.0.0"
-                assertTrue("Root package.json must have override $react with version $requiredReactVersion, but $reactVersion found") {
+                assertTrue("Root package.json must override $react with version $requiredReactVersion, but $reactVersion found") {
                     reactVersion == requiredReactVersion
                 }
             }
