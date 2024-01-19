c1780e7e1ac5196b9508a72cb10b53cce240efe6
Vladimir Sukharev <vladimir.sukharev@jetbrains.com>
2024-01-19 13:23:28 +0100

    [K/N] Remove platform-dependent check from IrObjCOverridabilityCondition
    ^KT-57640 Fixed

--- compiler/ir/ir.objcinterop/src/org/jetbrains/kotlin/ir/objcinterop/IrObjCOverridablilityCondidtion.kt
+++ compiler/ir/ir.objcinterop/src/org/jetbrains/kotlin/ir/objcinterop/IrObjCOverridablilityCondidtion.kt
@@ -29,6 +29,7 @@ object IrObjCOverridabilityCondition : IrExternalOverridabilityCondition {
         subMember: IrOverridableMember
     ): IrExternalOverridabilityCondition.Result {
         if (superMember.name == subMember.name) { // Slow path:
+            // KT-57640: There's no necessity to implement platform-dependent overridability check for properties
             if (superMember is IrFunction && subMember is IrFunction) {
                 superMember.getExternalObjCMethodInfo()?.let { superInfo ->
                     val subInfo = subMember.getExternalObjCMethodInfo()
@@ -47,8 +48,6 @@ object IrObjCOverridabilityCondition : IrExternalOverridabilityCondition {
                         }
                     }
                 }
-            } else if (superMember.isExternalObjCClassProperty() && subMember.isExternalObjCClassProperty()) {
-                return IrExternalOverridabilityCondition.Result.OVERRIDABLE
             }
         }
 
