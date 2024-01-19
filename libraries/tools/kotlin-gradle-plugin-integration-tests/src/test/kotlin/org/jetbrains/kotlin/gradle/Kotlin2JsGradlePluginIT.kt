bd8e2f0dd92a4b031acb60a729a3521efa166076
Ilya Goncharov <ilya.goncharov@jetbrains.com>
2024-01-19 16:57:32 +0100

    [Gradle, JS] Use yarn=false for NPM related tests

--- libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/Kotlin2JsGradlePluginIT.kt
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/Kotlin2JsGradlePluginIT.kt
@@ -1003,7 +1003,15 @@ class Kotlin2JsIrGradlePluginIT : KGPBaseTest() {
     @DisplayName("public NPM dependencies are included into package.json")
     @GradleTest
     fun testPackageJsonWithPublicNpmDependencies(gradleVersion: GradleVersion) {
-        project("npm-dependencies", gradleVersion) {
+        project(
+            "npm-dependencies",
+            gradleVersion,
+            buildOptions = defaultBuildOptions.copy(
+                jsOptions = defaultBuildOptions.jsOptions?.copy(
+                    yarn = false
+                )
+            )
+        ) {
             build("jsJar") {
                 val archive = projectPath
                     .resolve("build/libs")
@@ -1193,7 +1201,15 @@ class Kotlin2JsIrGradlePluginIT : KGPBaseTest() {
     @DisplayName("npm overrides works")
     @GradleTest
     fun testNpmOverrides(gradleVersion: GradleVersion) {
-        project("kotlin-js-npm-overrides", gradleVersion) {
+        project(
+            "kotlin-js-npm-overrides",
+            gradleVersion,
+            buildOptions = defaultBuildOptions.copy(
+                jsOptions = defaultBuildOptions.jsOptions?.copy(
+                    yarn = false
+                )
+            )
+        ) {
             build("packageJson", "rootPackageJson", "kotlinNpmInstall") {
                 fun getPackageJson() =
                     projectPath.resolve("build/js")
