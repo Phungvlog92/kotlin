8c2410d094c3fc2f10c4cc30f892f45360f0a944
Pavel Kunyavskiy <kunyavskiy@gmail.com>
2024-01-19 13:20:33 +0100

    [Fir2IR] Don't add invisible fake overrides to Lazy class
    ^KT-65056

--- compiler/tests-gen/org/jetbrains/kotlin/codegen/LightAnalysisModeTestGenerated.java
+++ compiler/tests-gen/org/jetbrains/kotlin/codegen/LightAnalysisModeTestGenerated.java
@@ -16031,6 +16031,11 @@ public class LightAnalysisModeTestGenerated extends AbstractLightAnalysisModeTes
             runTest("compiler/testData/codegen/box/fakeOverride/intersectionOverrideBetweenValAndVar.kt");
         }
 
+        @TestMetadata("kjkWithPackagePrivate.kt")
+        public void testKjkWithPackagePrivate() throws Exception {
+            runTest("compiler/testData/codegen/box/fakeOverride/kjkWithPackagePrivate.kt");
+        }
+
         @TestMetadata("kt49371.kt")
         public void testKt49371() throws Exception {
             runTest("compiler/testData/codegen/box/fakeOverride/kt49371.kt");
